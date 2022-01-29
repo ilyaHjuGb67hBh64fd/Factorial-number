b4450576dbe80d08d33e946d25c3798a8f9e5be5
ilmanuser <ilyag87@mail.ru>
2022-01-30 00:59:13 +0300

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int fact(int n)
 {
-    if(n < 0)  /// При отрицательном число
+    if(n < 0)  /// При отрицательном числе // if nagative number
 	
-        return 0;  // Вернется ноль
+        return 0;  // Вернется ноль // callback 0
 		
-    if (n == 0) // При нуле
+    if (n == 0) // При нуле if 0
 	
-        return 1; //  Вернется единица
+        return 1; //  Вернется единица // callback 1
 		
     else // Для всех других случаях
 	
-        return n * fact(n - 1); // Выполняем рекурсию.
-}
+        return n * fact(n - 1); // Выполняем рекурсию. // return formula of factorial recursion 
+} 
 
 int main()
 {
